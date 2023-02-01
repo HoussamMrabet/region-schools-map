f47e3b0cbea3220ca2fa65538a8e6b5b119b8b77
Mahmoud-Khairi <khairimahmoud01@gmail.com>
2023-02-01 19:37:17 +0100

    schoolproject

---
+++ test.js
@@ -0,0 +1,542 @@
+const api = [
+    {
+        "id": "birejdid",
+        "region": "لبير الجديد",
+        "public": {
+            "primaire": [
+                {
+                    "name": "مدرسة السنابل",
+                    "imageLink": "image/"
+                },
+                {
+                    "name": "مدرسة الداخلة",
+                    "imageLink": "image/الداخلة.png"
+                },
+                {
+                    "name": "مدرسة الفتح",
+                    "imageLink": "image/"
+                },
+                {
+                    "name": "مدرسة التهديب",
+                    "imageLink": "image/"
+                },
+                {
+                    "name": "مدرسة وادي المخازن",
+                    "imageLink": "image/المخازن.png"
+                }
+            ],
+            "college": [
+                {
+                    "name": "ثانوية الساحل الاعدادية",
+                    "imageLink": ""
+                },
+                {
+                    "name": " ثانوية علال الفاسي الاعدادية",
+                    "imageLink": "image/"
+                },
+                {
+                    "name": " ثانوية العيون الاعدادية",
+                    "imageLink": "image/"
+                }
+            ],
+            "lycee": [
+                {
+                    "name": "ثانوية عقبة بن نافع التأهيلية",
+                    "imageLink": "image/"
+                },
+                {
+                    "name": "ثانوية الإمام البخاري التأهيلية",
+                    "imageLink": "image/"
+                }
+            ]
+        },
+        "prive": {
+            "premiere": [
+                {
+                    "name": "العرفان",
+                    "imageLink": "image/"
+                },
+                {
+                    "name": "التقدم",
+                    "imageLink": "image/"
+                },
+                {
+                    "name": "مفتاح النور",
+                    "imageLink": "image/"
+                },
+                {
+                    "name": "ميثاق العرفان",
+                    "imageLink": "image/"
+                },
+                {
+                    "name": "سمول كاردن سكول",
+                    "imageLink": "image/"
+                }
+            ],
+            "primaire": [
+                {
+                    "name": "العرفان",
+                    "imageLink": "image/"
+                },
+                {
+                    "name": "التقدم",
+                    "imageLink": "image/"
+                },
+                {
+                    "name": "مفتاح النور",
+                    "imageLink": "image/"
+                },
+                {
+                    "name": "ميثاق العرفان",
+                    "imageLink": "image/"
+                },
+                {
+                    "name": "سمول كاردن سكول",
+                    "imageLink": "image/"
+                },
+                {
+                    "name": "أم هاجر",
+                    "imageLink": "image/"
+                }
+            ],
+            "college": [
+                {
+                    "name": "اع.العرفان",
+                    "imageLink": "image/"
+                },
+                {
+                    "name": "التقدم",
+                    "imageLink": "image/"
+                },
+                {
+                    "name": "مفتاح النور",
+                    "imageLink": "image/"
+                },
+                {
+                    "name": "ميثاق العرفان",
+                    "imageLink": "image/"
+                }
+            ],
+            "lycee": [
+                {
+                    "name": "التقدم",
+                    "imageLink": "image/"
+                },
+                {
+                    "name": "المفتاح النورتقدم",
+                    "imageLink": "image/"
+                },
+                {
+                    "name": "ميثاق العرفان",
+                    "imageLink": "image/"
+                }
+            ]
+        }
+    },
+    {
+        "id": "alibenyoussef",
+        "region": "أولاد سيدي علي بن يوسف",
+        "public": {
+            "primaire": [
+                {
+                    "name": "م/م العوجات",
+                    "imageLink": "image/"
+                },
+                {
+                    "name": "م/م أولا زيد",
+                    "imageLink": "image/"
+                }
+            ],
+            "college": [
+                {
+                    "name": "ثانوية عبد الله العروي الاعدادية",
+                    "imageLink": "image/"
+                }
+            ],
+            "lycee": [
+                {
+                    "name": "",
+                    "imageLink": ""
+                }
+            ]
+        },
+        "prive": {
+            "premiere": [
+                {
+                    "name": "",
+                    "imageLink": ""
+                }
+            ],
+            "primaire": [
+                {
+                    "name": "",
+                    "imageLink": ""
+                }
+            ],
+            "college": [
+                {
+                    "name": "",
+                    "imageLink": ""
+                }
+            ],
+            "lycee": [
+                {
+                    "name": "",
+                    "imageLink": ""
+                }
+            ]
+        }
+    },
+    {
+        "id": "mettouh",
+        "region": "متوح",
+        "public": {
+            "primaire": [
+                {
+                    "name": "مدرسة خميس متوح",
+                    "imageLink": "image/"
+                },
+                {
+                    "name": "م/م القياش",
+                    "imageLink": "image/"
+                },
+                {
+                    "name": "م/م لحميدات",
+                    "imageLink": "image/"
+                },
+                {
+                    "name": "م/م بوعلالة",
+                    "imageLink": "image/"
+                },
+                {
+                    "name": "م/م بير الكوارة",
+                    "imageLink": "image/"
+                }
+            ],
+            "college": [
+                {
+                    "name": "ثانوية عبد المالك السعدي الاعدادية",
+                    "imageLink": "image/"
+                }
+            ],
+            "lycee": [
+                {
+                    "name": " ثانوية موسى ابن نصير التأهيلية",
+                    "imageLink": "image/"
+                }
+            ]
+        },
+        "prive": {
+            "premiere": [
+                {
+                    "name": "",
+                    "imageLink": ""
+                }
+            ],
+            "primaire": [
+                {
+                    "name": "",
+                    "imageLink": ""
+                }
+            ],
+            "college": [
+                {
+                    "name": "",
+                    "imageLink": ""
+                }
+            ],
+            "lycee": [
+                {
+                    "name": "",
+                    "imageLink": ""
+                }
+            ]
+        }
+    },
+    {
+        "id": "boulaawan",
+        "region": "بولعوان",
+        "public": {
+            "primaire": [
+                {
+                    "name": "مدرسة بولعوان الجماعاتية",
+                    "imageLink": "image/"
+                },
+                {
+                    "name": "م/م الحيرش",
+                    "imageLink": "image/"
+                },
+                {
+                    "name": "م/م الخريطات",
+                    "imageLink": "image/"
+                },
+                {
+                    "name": "م/م بولعوان",
+                    "imageLink": "image/"
+                }
+            ],
+            "college": [
+                {
+                    "name": "ثانوية القصيبة الاعدادية",
+                    "imageLink": "image/"
+                }
+            ],
+            "lycee": [
+                {
+                    "name": " ",
+                    "imageLink": ""
+                }
+            ]
+        },
+        "prive": {
+            "premiere": [
+                {
+                    "name": "",
+                    "imageLink": ""
+                }
+            ],
+            "primaire": [
+                {
+                    "name": "",
+                    "imageLink": ""
+                }
+            ],
+            "college": [
+                {
+                    "name": "",
+                    "imageLink": ""
+                }
+            ],
+            "lycee": [
+                {
+                    "name": "",
+                    "imageLink": ""
+                }
+            ]
+        }
+    },
+    {
+        "id": "zawiatlekwassem",
+        "region": "زاوية لقواسم",
+        "public": {
+            "primaire": [
+                {
+                    "name": "م/م القواسم",
+                    "imageLink": "image/"
+                },
+                {
+                    "name": "م/م الصمامدة",
+                    "imageLink": "image/"
+                },
+                {
+                    "name": "م/م بوخنين",
+                    "imageLink": "image/"
+                }
+            ],
+            "college": [
+                {
+                    "name": "",
+                    "imageLink": ""
+                }
+            ],
+            "lycee": [
+                {
+                    "name": " ",
+                    "imageLink": ""
+                }
+            ]
+        },
+        "prive": {
+            "premiere": [
+                {
+                    "name": "",
+                    "imageLink": ""
+                }
+            ],
+            "primaire": [
+                {
+                    "name": "",
+                    "imageLink": ""
+                }
+            ],
+            "college": [
+                {
+                    "name": "",
+                    "imageLink": ""
+                }
+            ],
+            "lycee": [
+                {
+                    "name": "",
+                    "imageLink": ""
+                }
+            ]
+        }
+    },
+    {
+        "id": "mekres",
+        "region": "مكرس",
+        "public": {
+            "primaire": [
+                {
+                    "name": "م/م صقر",
+                    "imageLink": "image/"
+                },
+                {
+                    "name": "م/م سيدي بوعلام",
+                    "imageLink": "image/"
+                },
+                {
+                    "name": "م/م المساعدة",
+                    "imageLink": "image/"
+                },
+                {
+                    "name": "م/م القيايلة",
+                    "imageLink": "image/"
+                }
+            ],
+            "college": [
+                {
+                    "name": "",
+                    "imageLink": ""
+                }
+            ],
+            "lycee": [
+                {
+                    "name": " ",
+                    "imageLink": ""
+                }
+            ]
+        },
+        "prive": {
+            "premiere": [
+                {
+                    "name": "",
+                    "imageLink": ""
+                }
+            ],
+            "primaire": [
+                {
+                    "name": "",
+                    "imageLink": ""
+                }
+            ],
+            "college": [
+                {
+                    "name": "",
+                    "imageLink": ""
+                }
+            ],
+            "lycee": [
+                {
+                    "name": "",
+                    "imageLink": ""
+                }
+            ]
+        }
+    },
+    {
+        "id": "awladahssine",
+        "region": "أولاد احسين",
+        "public": {
+            "primaire": [
+                {
+                    "name": "م/م الطواجنة",
+                    "imageLink": "image/"
+                },
+                {
+                    "name": "م/م المنفلوطي",
+                    "imageLink": "image/"
+                },
+                {
+                    "name": "م/م أولاد احسين",
+                    "imageLink": "image/"
+                },
+                {
+                    "name": "م/م علي السبيطي",
+                    "imageLink": "image/"
+                },
+                {
+                    "name": "م/م المعاشات",
+                    "imageLink": "image/"
+                },
+                {
+                    "name": "م/م دار بلكندولية",
+                    "imageLink": "image/"
+                }
+            ],
+            "college": [
+                {
+                    "name": "",
+                    "imageLink": ""
+                }
+            ],
+            "lycee": [
+                {
+                    "name": " ",
+                    "imageLink": ""
+                }
+            ]
+        },
+        "prive": {
+            "premiere": [
+                {
+                    "name": "",
+                    "imageLink": ""
+                }
+            ],
+            "primaire": [
+                {
+                    "name": "",
+                    "imageLink": ""
+                }
+            ],
+            "college": [
+                {
+                    "name": "",
+                    "imageLink": ""
+                }
+            ],
+            "lycee": [
+                {
+                    "name": "",
+                    "imageLink": ""
+                }
+            ]
+        }
+    }
+]
+
+const paths = document.querySelectorAll("path");
+let y=0;
+paths.forEach(path => {
+    api.forEach(x=> {
+        if( x.id == path.id){
+            // console.log("khdam" + y + x.id + path.id);
+            // y++;
+            let div = document.createElement("div");
+            let popup = document.createElement("dialog")
+            let text = document.createTextNode("kheddam");
+            div.appendChild(popup)
+            popup.appendChild(text);
+            document.body.appendChild(div);
+        }
+    })
+});
+
+// let div = document.createElement("div");
+// let h1 = document.createElement("h1");
+// // let popup = document.createElement("dialog")
+// let text = document.createTextNode("kheddam");
+// // div.appendChild(popup)
+// // popup.appendChild(text);
+// h1.appendChild(text);
+// div.appendChild(h1);
+// document.body.appendChild(div);
+
+// let h1 = document.createElement("h1");
+// let h1Text = document.createTextNode("My App");
+// h1.appendChild(h1Text);
+// document.body.appendChild(h1);
+
+// appenchild kat3tiha weld kikon wastha
